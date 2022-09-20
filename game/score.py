928e70d14d067edac0b190f7ac89dd0d4a7047bf
jlane8 <113236803+jlane8@users.noreply.github.com>
2022-09-20 01:22:19 -0500

    commented throughout

--- game/score.py
+++ game/score.py
@@ -1,8 +1,25 @@
+"""
+File: score.py
+Author: Jerry Lane
+Purpose: This class will hold the score of a player, adjust the 
+score based on correct or incorrect bool values, and return the
+current score on request.
+"""
+
+# score class declaration
 class Score:
+    """
+    Parameters: none
+    Return: nothing
+    This class represents the score and score manipulations of a
+    given player.
+    """
     
+    # constructor
     def __init__(self):
         self.score = 300
 
+    # method to score the choice and amend the object's score
     def choice(self, is_correct):
         if is_correct:
             self.score += 100
@@ -12,5 +29,6 @@ class Score:
             self.score = 0
         return self.score
 
+    # method to return current score value
     def get_score(self):
         return self.score
